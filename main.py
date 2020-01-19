1a48ae9873fcb1247a2a0dfb794f8a2a8ebc4a82
potato <potato@potatos-MacBook-Pro.local>
2020-01-18 17:57:33 -0800

    deleted unnecessary code from main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import jinja2
 import json
 from datetime import datetime
 from datetime import timedelta
-from google.appengine.api import users
-from google.appengine.ext import ndb
+
 
 
 jinja_current_dir = jinja2.Environment(
@@ -14,40 +13,11 @@ jinja_current_dir = jinja2.Environment(
     autoescape=True)
 
 
-def get_candidates(prefix):
-  results = []
-  if len(prefix) == 0:
-    return results
-  for candidates in Candidate:
-    if candidates.lower().startswith(prefix.lower()):
-      results.append(candidates)
-    return results
-
-  def get_events(prefix):
-    results = []
-    if len(prefix) == 0:
-      return results
-    for events in EVENTS:
-      if events.lower().startswith(prefix.lower()):
-        results.append(events)
-        return results
-    return results
-
-class SeedDataHandler(webapp2.RequestHandler):
-    def get(self):
-        seed_data()
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/CandidateList', CandidateHandler),
-    ("/calendar", CalendarHandler),
-    ("/events", EventHandler),
-    ('/blogpost', BlogPostHandler),
-    ('/login', LoginPageHandler),
-    ('/afterpost', AfterPostHandler),
-    ('/blogpostlist', BlogPostListHandler),
-    ("/seed_data", SeedDataHandler),
+
 
 ], debug=True)
